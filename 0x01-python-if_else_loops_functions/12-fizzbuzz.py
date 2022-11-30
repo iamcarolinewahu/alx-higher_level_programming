d52e98860d1b85c3141fc6b9e196fdcb091c6b43
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-11-30 11:49:34 -0800

    Editted the function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,8 +7,8 @@ def fizzbuzz():
         if (num % 3) == 0 and (num % 5) == 0:
             print("FizzBuzz ", end='')
         elif (num % 3) == 0:
-            print("Buzz ", end='')
-        elif (num % 5) == 0:
             print("Fizz ", end='')
+        elif (num % 5) == 0:
+            print("Buzz ", end='')
         else:
             print(f'{num} ', end='')
