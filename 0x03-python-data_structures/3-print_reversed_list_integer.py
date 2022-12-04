57b19e0494a2c66c639ca5d208fc837a5acbcc1e
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-04 03:35:01 -0800

    added isinstance function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list = my_list.reverse()
-    for val in my_list:
-        print("{:d}".format(val))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for val in my_list:
+            print("{:d}".format(val))
