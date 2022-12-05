2c03cbea85b10da85cfd5ab6dd49b5e6c5673520
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-05 09:00:52 -0800

    Modified the del method

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    new_list = my_list.remove(my_list[idx])
     if idx < 0 and idx >= len(my_list):
         return my_list
-    return new_list
+    del my_list[idx]
+    return my_list
