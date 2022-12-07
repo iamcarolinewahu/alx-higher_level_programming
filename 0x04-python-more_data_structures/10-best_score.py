5d722758723c536fbe2e8f2cd31ee018cc1b4b9d
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-07 13:11:13 -0800

    Modified function

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,4 +5,4 @@ def best_score(a_dictionary):
         return None
     v = list(a_dictionary.values())
     k = list(a_dictionary.keys())
-    return k[v.index[max(v))]
+    return k[v.index(max(v))]
