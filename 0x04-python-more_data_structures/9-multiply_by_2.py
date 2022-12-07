d561c3c4a87ed87631e1f90c5ce0f5cf2ae30f1d
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-07 08:26:33 -0800

    Modified function

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,7 @@
 
 def multiply_by_2(a_dictionary):
     if a_dictionary is not None:
-        new_dict = a_dictionary
+        new_dict = dict(a_dictionary)
         for n, m in new_dict.items():
             new_dict[n] = m * 2
         return new_dict
