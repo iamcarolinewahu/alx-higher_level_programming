0c88b5c4cd07819182f317a1eefc48b4d25c9cd8
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-07 11:03:01 -0800

    Modified function

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_dict = a_dictionary
+    new_dict = dict(a_dictionary)
     for i, j in new_dict.items():
         if j == value:
             new_dict.pop(i)
