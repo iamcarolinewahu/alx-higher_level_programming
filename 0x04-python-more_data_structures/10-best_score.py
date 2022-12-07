2da23daa9b9a62a0409157d58e4ebd7a7ef3f21e
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-07 12:10:46 -0800

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,7 @@
 def best_score(a_dictionary):
     if a_dictionary is not None:
         new_dict = max(a_dictionary.value())
-    return new_dict
+        return new_dict
+    else:
+        return None
+
