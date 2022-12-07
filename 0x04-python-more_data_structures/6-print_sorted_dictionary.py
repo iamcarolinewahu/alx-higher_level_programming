0d9a737c3fc3eda6e6d24e600c25fe0b8f649cd6
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-07 07:32:03 -0800

    modified function

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None:
         sorted_keys = sorted(a_dictionary.keys())
-        for k in keys:
+        for k in sorted_keys:
             print("{:s}: {}".format(k, a_dictionary[k]))
