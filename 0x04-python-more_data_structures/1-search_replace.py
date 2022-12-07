9ccf2a4d4b34fa5b36eadd429fd3cf2c5cd9dc46
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-07 01:09:26 -0800

    modified function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = [:]
+    new_list = my_list[:]
     for element in range(len(new_list)):
         if new_list == search:
             search = replace
