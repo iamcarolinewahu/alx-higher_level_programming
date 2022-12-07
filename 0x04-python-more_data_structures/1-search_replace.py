29d2624fe1d495fb554d8ca7c880934ba3beea1c
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-07 01:07:38 -0800

    modified spelling mistake

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 
 def search_replace(my_list, search, replace):
     new_list = [:]
-    for element in len(new_list):
+    for element in range(len(new_list)):
         if new_list == search:
             search = replace
     return new_list
