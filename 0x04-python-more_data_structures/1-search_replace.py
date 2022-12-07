87e600c9636dc6746ff377230fd26388b126f950
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-12-07 01:11:42 -0800

    modified function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for element in range(len(new_list)):
-        if new_list == search:
-            search = replace
+        if new_list[element] == search:
+            new_list[element] = replace
     return new_list
