2777e7a0523536fe427089904a3044838b614d6a
iamcarolinewahu <caroline.wahu254@gmail.com>
2023-01-18 06:24:38 -0800

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+adds items to json file
+"""
+
+import sys
+import json
+
+
+load = __import__('6-load_from_json_file.py').load_from_json_file
+save = __import__('5-save_to_json_file.py').save_to_json_file
+args = sys.argv[1:]
+jsonfile = "add_item.json"
+try:
+    my_list = load(jsonfile)
+except FileNotFoundError:
+    save(args, jsonfile)
+else:
+    my_list.extend(args)
+    save(my_list, jsonfile)
