c5ec66d606453da9934643823edfb2321731670e
iamcarolinewahu <caroline.wahu254@gmail.com>
2023-01-18 01:47:29 -0800

    changed mode in function

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,5 +9,5 @@ def append_write(filename="", text=""):
     function that appends a string at the end of a file
     and return the number of characters
     """
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
