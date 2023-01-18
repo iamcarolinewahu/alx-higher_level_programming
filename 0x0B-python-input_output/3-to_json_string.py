6591851b21577a811f1483533222d323d45179f3
iamcarolinewahu <caroline.wahu254@gmail.com>
2023-01-18 01:50:54 -0800

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+returns json representation of a string
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function to convert json to string
+    """
+
+    return json.dumps(my_obj)
