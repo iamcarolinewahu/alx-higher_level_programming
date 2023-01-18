eb20f8324a002d58356a66451cefe2575322d133
iamcarolinewahu <caroline.wahu254@gmail.com>
2023-01-18 00:57:28 -0800

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+prints contents of a text file
+"""
+
+
+def read_file(filename=""):
+    """
+    function to read a file
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
