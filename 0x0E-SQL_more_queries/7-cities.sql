5887bbac26b4e109a8c85eea801e3de93ccba86c
iamcarolinewahu <caroline.wahu254@gmail.com>
2023-02-15 11:27:38 -0800

    modified script

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
+    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
     state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
