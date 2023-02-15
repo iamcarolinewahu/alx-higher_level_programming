b4934efb9a4368bedcb5c5ec29788f205a4ad3aa
iamcarolinewahu <caroline.wahu254@gmail.com>
2023-02-15 10:28:32 -0800

    modified script

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- creates the table unique_id
 -- with values id and name
 -- id should have default value 1 and must be unique
-CREATE TABLE IF NOT EXISTS unique_iD (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
