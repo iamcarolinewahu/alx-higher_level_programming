a5a72385b5dd26c23ddc9c22654a6dcbb2568260
iamcarolinewahu <caroline.wahu254@gmail.com>
2023-02-15 11:01:15 -0800

    modified script

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,3 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (ID INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (ID INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
